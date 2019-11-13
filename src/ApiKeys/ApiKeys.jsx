3f359cabfbc2a3ec5f20f2cb55930d43768a43bd
Сергей Андерсон <50884398+BARADA@users.noreply.github.com>
2019-11-13 19:12:17 +0300

    Update ApiKeys.jsx

--- src/ApiKeys/ApiKeys.jsx
+++ src/ApiKeys/ApiKeys.jsx
@@ -1,3 +1,3 @@
-export const OpenWeatherMapKey = '053124b70fdd14d21929c236ad14d398';
-export const APIXUKey = '773d5e2443e14ce885b161201191506';
-export const GeocodingKey = '637489241c01de';
+export const OpenWeatherMapKey = '1111111111111111111111111111';
+export const APIXUKey = '1111111111111111111111111111';
+export const GeocodingKey = '1111111111111111111111';
